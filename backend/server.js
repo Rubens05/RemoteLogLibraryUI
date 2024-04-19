f955c2fc3ee10c2cb7e856a95a3c2fd69645d005
Rubens05 <rubenggjac@hotmail.com>
2024-04-19 16:00:36 +0200

    Time filter

--- backend/server.js
+++ backend/server.js
@@ -32,38 +32,28 @@ const Log = mongoose.model('Log', logSchema, "LogRegistry"); // 'Log' es el nomb
 
 // Endpoint para obtener los logs
 app.get('/api', async (req, res) => {
+    const { startDate, endDate } = req.query;
     try {
-        const logs = await Log.find().sort({ timestamp: -1 }).limit(500);
-        console.log("Logs fetched:", logs);  // Debug: Imprimir los logs obtenidos
+        const query = {};
+        if (startDate && endDate) {
+            const start = new Date(startDate);
+            const end = new Date(endDate);
+            start.setDate(start.getDate()); // Incluir todo el día de la fecha inicial
+            end.setDate(end.getDate() + 1); // Incluir todo el día de la fecha final
+            query.timestamp = { $gte: start, $lte: end };
+            console.log("Querying logs between", start, "and", end);
+        }
+        const logs = await Log.find(query).sort({ timestamp: -1 }).limit(500);
+        console.log("logs", logs)
         res.json({ logs });
     } catch (error) {
-        console.error("Error fetching logs:", error);  // Debug: Imprimir cualquier error
+        console.error("Error fetching logs:", error);
         res.status(500).json({ message: error.message });
     }
 });
 
+
+
 app.listen(5000, () => {
     console.log('Servidor en ejecución en el puerto 5000');
 });
-
-
-// // Función para insertar un nuevo registro en la base de datos
-// async function insertLog(level, message, idSender, topic, timestamp) {
-//     try {
-//         // Crear una nueva instancia del modelo Log con los datos proporcionados
-//         const newLog = new Log({ level, message, idSender, topic, timestamp });
-
-//         // Guardar el nuevo registro en la base de datos
-//         await newLog.save();
-//         console.log('Registro insertado correctamente:', newLog);
-
-//         return newLog; // Devolver el nuevo registro insertado
-//     } catch (error) {
-//         console.error('Error al insertar el registro:', error);
-//         throw error; // Propagar el error para manejarlo en otro lugar si es necesario
-//     }
-// }
-
-// // Llamar a la función insertLog para insertar un nuevo registro
-// insertLog("DEBUG", "Debug en la placa del usuario", "defaultIdBoard", "defaultTopic", newDate("2024-04-17T14:32:00Z");
-
